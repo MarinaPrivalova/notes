eeab49606c693be6d9de43adde0a09bbd3d92b19
MarinaPrivalova <privalovama@yandex.ru>
2023-03-01 21:06:19 +0300

    feat: notes

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const keys = document.querySelectorAll('.notes__key');
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.notes__key[data-key="${e.keyCode}"]`);
+  if(!audio) return; /**остановить функцию*/
+  audio.currentTime = 0; /**перемотать к началу*/
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; /**пропустить, если не transform*/
+  this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
